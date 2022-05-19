feb2751c6b4cd3afb74edd88add5d8614cab55f6
Satyadeo <satyadeo@outlook.in>
2022-05-19 12:49:33 +0530

    few programs

--- Sort_words_from_string.py
+++ Sort_words_from_string.py
@@ -1,4 +1,4 @@
-#Write a code which accepts a sequence of words as input and prints the words in a sequence after sorting them alphabetically.
+#Write a code which accepts a sequence of words as input and prints the words in a sequence after sorting them alphabetically
 
 import math
 
